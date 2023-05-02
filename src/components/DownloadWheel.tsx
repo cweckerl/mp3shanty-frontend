f536801f29c69b467060034ac1437cbbc6f553ba
cweckerl <c4rl.hawks@gmail.com>
2023-05-01 22:42:32 -0700

    Prevent download wheel from being dragged

--- src/components/DownloadWheel.tsx
+++ src/components/DownloadWheel.tsx
@@ -1,6 +1,7 @@
 export const DownloadWheel = () => {
   return <img
     className='download-wheel'
+    draggable='false'
     src={'https://raw.githubusercontent.com/cweckerl/mp3shanty-frontend/master/public/downloading.gif'}
     alt='Downloading'
     height='50'
