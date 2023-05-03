372b20c2594bafbb3fbdd37c5d00a6c6cd40284a
cweckerl <c4rl.hawks@gmail.com>
2023-05-02 22:08:24 -0700

    Resize wheel

--- src/components/DownloadWheel.tsx
+++ src/components/DownloadWheel.tsx
@@ -4,6 +4,5 @@ export const DownloadWheel = () => {
     draggable='false'
     src={'https://raw.githubusercontent.com/cweckerl/mp3shanty-frontend/master/public/downloading.gif'}
     alt='Downloading'
-    height='50'
   />
 }
